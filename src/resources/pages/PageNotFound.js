52d0f3c5b1580eb261c071127ec39c93b94b4dcd
Reenjay <reenjie17@gmail.com>
2023-02-07 11:38:19 +0800

    commit

---
+++ src/resources/pages/PageNotFound.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import '../../assets/css/pagenotfound.css'
+
+function PageNotFound() {
+  return (
+    <div className='page'>
+        
+        <h1>
+        404 
+        </h1>
+        <span>Requested page not found</span>
+
+        
+    </div>
+  )
+}
+
+export default PageNotFound
--- src/routes/Web.js
+++ src/routes/Web.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import { Routes, Route, Outlet, useLocation, Navigate } from 'react-router-dom';
 import Homepage from '../resources/pages/Homepage';
+import Login from '../resources/pages/auth/Login';
+import PageNotFound from '../resources/pages/PageNotFound';
 function Web() {
   return (
     <Routes>
         <Route path="/" element={<Homepage/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
+        {/* <Route element={<AdminAuth />}>
+              <Route path="/admin" element={<AdminInfo />} />
+              <Route path="/adminview" element={<Admin />} />
+              <Route path="/role" element={<Role />} />
+            </Route> */}
+
+
+  <Route path="*" element={<PageNotFound />} />
     </Routes>
   )
 }
